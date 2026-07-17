fd6d3dfce4eb3ea8234f6d2d061fc9e49149c6a3
Titas <titaslr38@gmail.com>
2026-07-17 23:32:53 +0300

    Password entry data

---
+++ Models/PasswordEntry.cs
@@ -0,0 +1,8 @@
+namespace Password_manager.Models;
+
+public class PasswordEntry
+{
+    public string Website { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
