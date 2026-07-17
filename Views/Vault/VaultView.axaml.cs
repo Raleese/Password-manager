31e3eecc9fd5f0f20cbb2daf5f023e90b5ac0a14
Titas <titaslr38@gmail.com>
2026-07-17 23:32:17 +0300

    UI improvements

---
+++ Views/Vault/VaultView.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia.Controls;
+using Password_manager.Models;
+using Password_manager.ViewModels;
+
+namespace Password_manager.Views;
+
+public partial class VaultView : UserControl
+{
+    public VaultView()
+    {
+        InitializeComponent();
+    }
+
+    private void EditButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is VaultViewModel viewModel && sender is Button button && button.DataContext is PasswordEntry entry)
+        {
+            viewModel.EditPasswordEntryCommand.Execute(entry);
+        }
+    }
+
+    private void DeleteButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is VaultViewModel viewModel && sender is Button button && button.DataContext is PasswordEntry entry)
+        {
+            viewModel.DeletePasswordEntryCommand.Execute(entry);
+        }
+    }
+}
