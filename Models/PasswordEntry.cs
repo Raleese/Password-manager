ec2426744f2407de334f4cfadb3cddcaa5b80f70
Titas <titaslr38@gmail.com>
2026-07-18 16:07:35 +0300

    Added ID

--- Models/PasswordEntry.cs
+++ Models/PasswordEntry.cs
@@ -2,6 +2,7 @@ namespace Password_manager.Models;
 
 public class PasswordEntry
 {
+    public int Id { get; set; }
     public string Website { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
