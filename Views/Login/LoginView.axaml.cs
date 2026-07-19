ce595928f22123d049dfcc2bdd971de75456622f
Titas <titaslr38@gmail.com>
2026-07-20 01:11:29 +0300

    Login screen view

---
+++ Views/Login/LoginView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Password_manager.Views;
+
+public partial class LoginView : UserControl
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+}
