0ebe0743a0dd6e0aa6b1a33b05ce327d91c31e33
Titas <titaslr38@gmail.com>
2026-07-20 16:53:43 +0300

    Changed password from string to BLOB for security purposes

--- Models/PasswordEntry.cs
+++ Models/PasswordEntry.cs
@@ -5,5 +5,5 @@ public class PasswordEntry
     public int Id { get; set; }
     public string Website { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public byte[] Password { get; set; } = [];
 }
